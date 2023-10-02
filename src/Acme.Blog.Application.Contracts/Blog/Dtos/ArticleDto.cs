1ceeb41514eb1083f4324128b76d4bcddb7d2f3e
kon1z <zzx2867199593@163.com>
2023-10-03 00:06:24 +0800

    chore 优化代码

--- src/Acme.Blog.Application.Contracts/Blog/Dtos/ArticleDto.cs
+++ src/Acme.Blog.Application.Contracts/Blog/Dtos/ArticleDto.cs
@@ -5,7 +5,7 @@ namespace Acme.Blog.AppServices;
 
 public class ArticleDto : EntityDto<Guid>
 {
-    public string Title { get; set; }
-    public string Description { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
     public DateTime CreateTime { get; set; }
 }
--- src/Acme.Blog.HttpApi/Models/Test/TestModel.cs
+++ src/Acme.Blog.HttpApi/Models/Test/TestModel.cs
@@ -4,7 +4,7 @@ namespace Acme.Models.Test;
 
 public class TestModel
 {
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
 
     public DateTime BirthDate { get; set; }
 }
