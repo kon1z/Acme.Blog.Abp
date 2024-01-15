cabfd96db378b313e535467d1ed21e12afca80db
cquan123 <cquan6615@gmail.com>
2024-01-15 23:01:03 +0800

    init

---
+++ src/Acme.Blog.Domain/Blog/Entities/Category.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Acme.Blog.Entities
+{
+    public  class Category : FullAuditedAggregateRoot<Guid>
+    {
+        public string Name { get; set; }
+    }
+}
